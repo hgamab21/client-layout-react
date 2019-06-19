6885972e4d46f647c55c2c23d20349f851f076cd
Hiram Abraham <hiramabraham@Hirams-MacBook-Pro.local>
2019-06-19 12:09:57 -0500

    Completed hero section for desktop

--- client-layout/src/App.js
+++ client-layout/src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo_transparent';
+import './App.scss';
+//import hero from './assets/hero-splash.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="hero-section">
+
+        <div className="topnav">
+
+          <div className="logo-box">
+            <img src={logo} className="logo-control" alt="Logo"></img>
+          </div>
+
+          <Navbar />
+        </div>
+
+        <div className="clearfix"></div>
+
+        <HeroText text="THIS IS A HEADLINE."/>
+        </div>
+
+        <div className="content-section">
+
+          <div className="category-menu">
+
+          </div>
+
+        </div>
+    </div>
+  );
+}
+
+function Navbar(){
+  return(
+    <nav className="navbar">
+      <NavItem link="#" title="Home"/>
+      <NavItem link="#" title="About"/>
+      <NavItem link="#" title="Products"/>
+      <NavItem link="#" title="Help"/>
+    </nav>
+  );
+}
+
+function NavItem(props){
+  return <a href={props.link}>{props.title}</a>;
+}
+
+function HeroText(props){
+  return (
+    <div className="textbox">
+      <p className="hero-text">{props.text}</p>
     </div>
   );
 }
